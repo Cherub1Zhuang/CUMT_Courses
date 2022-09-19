4615d150c9f307f102a272a27894f7855f503003
Cherub1Zhuang <zxw@cumt.edu.cn>
2022-09-19 15:31:06 +0800

    整理目录

--- directory_manager.py
+++ directory_manager.py
@@ -16,6 +16,18 @@ def make_dir(path):
     os.makedirs(path3)
     os.makedirs(path4)
 
+def add_readme(path):
+    list=os.listdir(path)
+    for item in list:
+        path1=os.path.join(path,item)
+        list1=os.listdir(path1)
+        for item1 in list1:
+            path2=os.path.join(path1,item1,"Readme.md")
+            with open(path2,'w',encoding='utf-8') as f:
+                f.writelines("# Readme")
+                f.close()
+            print (path2)
+
 root=os.getcwd()
 list=os.listdir(root)
 print(root)
@@ -23,9 +35,10 @@ print(list)
 for item in list:
     if isChinese(item):
         file_path=os.path.join(root,item)
-        dir1=os.path.join(file_path,"本科生")
-        dir2=os.path.join(file_path,"研究生")
-        os.makedirs(dir1)
-        make_dir(dir1)
-        os.makedirs(dir2)
-        make_dir(dir2)
+        # dir1=os.path.join(file_path,"本科生")
+        # dir2=os.path.join(file_path,"研究生")
+        # os.makedirs(dir1)
+        # make_dir(dir1)
+        # os.makedirs(dir2)
+        # make_dir(dir2)
+        add_readme(file_path)
